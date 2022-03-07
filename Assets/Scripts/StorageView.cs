38824d09e46ece69222689197e4a1410195b3c1b
Yusuke Ishiguro <ishiguro_yusuke@cyberagent.co.jp>
2022-03-07 15:56:34 +0900

    unity: Added sample scene

---
+++ Assets/Scripts/StorageView.cs
@@ -0,0 +1,21 @@
+// --------------------------------------------------------------
+// Copyright 2022 CyberAgent, Inc.
+// --------------------------------------------------------------
+
+using MobileSupport;
+using UnityEngine;
+
+public class StorageView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+#if UNITY_ANDROID
+        Debug.LogFormat("Available disk space: {0}", Storage.GetInternalUsableSpace());
+        Debug.LogFormat("Available disk space (accurate): {0}", Storage.GetInternalUsableSpace(true));
+#endif
+#if UNITY_IOS
+        Debug.LogFormat("Available disk space: {0}", Storage.GetInternalUsableSpace());
+#endif
+    }
+}
