2d3f7b9447f792cb76f334472ad0f8105118e1c3
Yusuke Ishiguro <ishiguro_yusuke@cyberagent.co.jp>
2022-03-08 12:45:32 +0900

    added code document

--- Assets/MobileSupport/Runtime/Scripts/Storage.iOS.cs
+++ Assets/MobileSupport/Runtime/Scripts/Storage.iOS.cs
@@ -15,6 +15,10 @@ namespace MobileSupport
         [DllImport(DllName)]
         private static extern long storage_getFreeDiskSpace();
 
+        /// <summary>
+        /// Get usable space of internal storage.
+        /// </summary>
+        /// <returns>Bytes of usable space. It will return -1 for internal error and in Unity editor.</returns>
         private static long GetInternalUsableSpace()
         {
 #if UNITY_EDITOR
