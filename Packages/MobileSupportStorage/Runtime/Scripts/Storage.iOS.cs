4920cdfeac19e624ab64c575f03826fc6562f258
Yusuke Ishiguro <ishiguro_yusuke@cyberagent.co.jp>
2022-08-16 17:12:18 +0900

    storage: use new API to get free space including deletable caches

--- Packages/MobileSupportStorage/Runtime/Scripts/Storage.iOS.cs
+++ Packages/MobileSupportStorage/Runtime/Scripts/Storage.iOS.cs
@@ -13,13 +13,14 @@ namespace MobileSupport
         private const string DllName = "__Internal";
 
         [DllImport(DllName)]
-        private static extern long storage_getFreeDiskSpace();
+        private static extern long storage_getFreeDiskSpace(bool includeDeletableCaches);
 
         /// <summary>
         /// Get usable space of internal storage.
         /// </summary>
+        /// <param name="includeDeletableCaches">Set <c>true</c> to include size of deletable caches. Value showed at System are included.</param>
         /// <returns>Bytes of usable space. It will return -1 for internal error and in Unity editor.</returns>
-        public static long GetInternalUsableSpace()
+        public static long GetInternalUsableSpace(bool includeDeletableCaches = false)
         {
 #if UNITY_EDITOR
             if (Application.isEditor)
@@ -28,7 +29,7 @@ namespace MobileSupport
             }
 #endif
 
-            return storage_getFreeDiskSpace();
+            return storage_getFreeDiskSpace(includeDeletableCaches);
         }
 
     }
