60a3532268721949ae25226ff1b7cd6f7686ba34
ruccho <noboroo2000@yahoo.co.jp>
2024-07-03 17:30:58 +0900

    add comments

--- Packages/MobileSupportThermal/Runtime/Scripts/ThermalStatus.cs
+++ Packages/MobileSupportThermal/Runtime/Scripts/ThermalStatus.cs
@@ -11,57 +11,112 @@ namespace MobileSupport
     /// </summary>
     public readonly struct ThermalStatus
     {
-        public bool IsIOS => !_isAndroid;
-        public bool IsAndroid => _isAndroid;
-        
-        private readonly bool _isAndroid;
+        public bool IsIOS => !IsAndroid;
+        public bool IsAndroid { get; }
+
         private readonly int _status;
-        
+
         public ThermalStatus(ThermalStatusAndroid status)
         {
-            _isAndroid = true;
+            IsAndroid = true;
             _status = (int)status;
         }
-        
+
         public ThermalStatus(ThermalStatusIOS status)
         {
-            _isAndroid = false;
+            IsAndroid = false;
             _status = (int)status;
         }
-        
-        public static implicit operator ThermalStatus(ThermalStatusAndroid status) => new (status);
-        
-        public static implicit operator ThermalStatus(ThermalStatusIOS status) => new (status);
+
+        public static implicit operator ThermalStatus(ThermalStatusAndroid status)
+        {
+            return new ThermalStatus(status);
+        }
+
+        public static implicit operator ThermalStatus(ThermalStatusIOS status)
+        {
+            return new ThermalStatus(status);
+        }
 
         public ThermalStatusAndroid UnwrapAsAndroid()
         {
-            if(!_isAndroid) throw new InvalidOperationException("This status is not for Android.");
+            if (!IsAndroid) throw new InvalidOperationException("This status is not for Android.");
             return (ThermalStatusAndroid)_status;
         }
-        
+
         public ThermalStatusIOS UnwrapAsIOS()
         {
-            if(_isAndroid) throw new InvalidOperationException("This status is not for iOS.");
+            if (IsAndroid) throw new InvalidOperationException("This status is not for iOS.");
             return (ThermalStatusIOS)_status;
         }
     }
 
+    /// <summary>
+    ///     Equivalent to <see href="https://developer.android.com/reference/android/os/PowerManager">PowerManager</see>'s
+    ///     THERMAL_STATUS_* values.
+    /// </summary>
     public enum ThermalStatusAndroid
     {
+        /// <summary>
+        ///     Not under throttling.
+        /// </summary>
         None,
+
+        /// <summary>
+        ///     Light throttling where UX is not impacted.
+        /// </summary>
         Light,
+
+        /// <summary>
+        ///     Moderate throttling where UX is not largely impacted.
+        /// </summary>
         Moderate,
+
+        /// <summary>
+        ///     Severe throttling where UX is largely impacted.
+        /// </summary>
         Severe,
+
+        /// <summary>
+        ///     Platform has done everything to reduce power.
+        /// </summary>
         Critical,
+
+        /// <summary>
+        ///     Key components in platform are shutting down due to thermal condition. Device functionalities will be limited.
+        /// </summary>
         Emergency,
-        Shutdown,
+
+        /// <summary>
+        ///     Need shutdown immediately.
+        /// </summary>
+        Shutdown
     }
 
+    /// <summary>
+    ///     Equivalent to
+    ///     <see href="https://developer.apple.com/documentation/foundation/nsprocessinfothermalstate">NSProcessInfoThermalState</see>.
+    /// </summary>
     public enum ThermalStatusIOS
     {
+        /// <summary>
+        ///     The thermal state is within normal limits.
+        /// </summary>
         Nominal,
+
+        /// <summary>
+        ///     The thermal state is slightly elevated.
+        /// </summary>
         Fair,
+
+        /// <summary>
+        ///     The thermal state is high.
+        /// </summary>
         Serious,
+
+        /// <summary>
+        ///     The thermal state is significantly impacting the performance of the system and the device needs to cool down.
+        /// </summary>
         Critical
     }
 }
