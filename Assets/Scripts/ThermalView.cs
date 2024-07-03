bb63dd1b9d7303993d7926a7d4ed7031d8b759d8
ruccho <noboroo2000@yahoo.co.jp>
2024-07-03 17:10:18 +0900

    update ThermalView.cs

--- Assets/Scripts/ThermalView.cs
+++ Assets/Scripts/ThermalView.cs
@@ -14,28 +14,14 @@ public class ThermalView : MonoBehaviour
     private void Start()
     {
 #if UNITY_ANDROID
-        Thermal.OnBatteryTemperatureChanged += value =>
+        Thermal.Android.OnBatteryTemperatureChanged += value =>
         {
             Profiler.BeginSample("OnBatteryTemperatureChanged");
             Debug.Log($"Battery Temperature: {value}");
             Profiler.EndSample();
         };
 
-        Thermal.OnBatteryLevelChanged += levelPercentage =>
-        {
-            Profiler.BeginSample("OnBatteryLevelChanged");
-            Debug.Log($"Battery Level: {levelPercentage}");
-            Profiler.EndSample();
-        };
-
-        Thermal.OnBatteryStatusChanged += status =>
-        {
-            Profiler.BeginSample("OnBatteryStatusChanged");
-            Debug.Log($"Battery Level: {status}");
-            Profiler.EndSample();
-        };
-
-        Thermal.OnBatteryVoltageChanged += voltage =>
+        Thermal.Android.OnBatteryVoltageChanged += voltage =>
         {
             Profiler.BeginSample("OnBatteryVoltageChanged");
             Debug.Log($"Battery Voltage: {voltage} mV");
@@ -44,18 +30,28 @@ public class ThermalView : MonoBehaviour
 #endif
 
 #if UNITY_ANDROID || UNITY_IOS
+
+        Thermal.OnBatteryLevelChanged += level => { Debug.Log($"Battery Level: {level}"); };
+
+        Thermal.OnBatteryStatusChanged += status => { Debug.Log($"Battery Status: {status}"); };
+
         Thermal.OnThermalStatusChanged += status =>
         {
-            Profiler.BeginSample("OnThermalStatusChanged");
-            Debug.Log($"Thermal Status: {status}");
-            Profiler.EndSample();
+#if UNITY_ANDROID
+            Debug.Log($"Thermal Status: {status.UnwrapAsAndroid()}");
+#elif UNITY_IOS
+            Debug.Log($"Thermal Status: {status.UnwrapAsIOS()}");
+#endif
         };
         Thermal.StartMonitoring();
 #endif
 
+#if UNITY_ANDROID
         StartCoroutine(GetValuesLooped());
+#endif
     }
 
+#if UNITY_ANDROID
     private IEnumerator GetValuesLooped()
     {
         Stopwatch sw = new();
@@ -65,39 +61,51 @@ public class ThermalView : MonoBehaviour
         {
             yield return new WaitForSeconds(0.5f);
 
-#if UNITY_ANDROID
             sw.Restart();
-            Profiler.BeginSample("GetThermalHeadroom");
-            Thermal.GetThermalHeadroom(0, out var headroom, out var resultForecastSeconds, out var isLatestValue);
+            Profiler.BeginSample("Android.GetThermalHeadroom");
+            Thermal.Android.GetThermalHeadroom(0, out var headroom, out var resultForecastSeconds,
+                out var isLatestValue);
             Profiler.EndSample();
             elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
 
             Debug.Log(
                 $"Thermal Headroom: {headroom}, isLatestValue: {isLatestValue}, resultForecastSeconds: {resultForecastSeconds} (obtained in {elapsed} ms)");
-#endif
 
             sw.Restart();
-            var counter = Thermal.GetBatteryEnergyCounter();
-            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
-
-            sw.Restart();
-            var currentNow = Thermal.GetBatteryCurrentNow();
-            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
-
-            sw.Restart();
-            var capacity = Thermal.GetBatteryCapacity();
-            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
-
-            sw.Restart();
-            var chargeCounter = Thermal.GetBatteryChargeCounter();
-            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
-
-            sw.Restart();
-            var currentAverage = Thermal.GetBatteryCurrentAverage();
+            var counter = Thermal.Android.GetBatteryEnergyCounter();
             elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
 
             Debug.Log(
-                $"Energy Counter: {counter}, Current Now: {currentNow}, Capacity: {capacity}, Charge Counter: {chargeCounter}, Current Average: {currentAverage} (obtained in {elapsed} ms)");
+                $"Energy Counter: {counter} (obtained in {elapsed} ms)");
+
+            sw.Restart();
+            var currentNow = Thermal.Android.GetBatteryCurrentNow();
+            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
+
+            Debug.Log(
+                $"Current Now: {currentNow} (obtained in {elapsed} ms)");
+
+            sw.Restart();
+            var capacity = Thermal.Android.GetBatteryCapacity();
+            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
+
+            Debug.Log(
+                $"Capacity: {capacity} (obtained in {elapsed} ms)");
+
+            sw.Restart();
+            var chargeCounter = Thermal.Android.GetBatteryChargeCounter();
+            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
+
+            Debug.Log(
+                $"Charge Counter: {chargeCounter} (obtained in {elapsed} ms)");
+
+            sw.Restart();
+            var currentAverage = Thermal.Android.GetBatteryCurrentAverage();
+            elapsed = (double)sw.ElapsedTicks / Stopwatch.Frequency * 1000;
+
+            Debug.Log(
+                $"Current Average: {currentAverage} (obtained in {elapsed} ms)");
         }
     }
+#endif
 }
